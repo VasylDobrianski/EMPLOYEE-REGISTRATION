3632ce1feb014d5b17968c9c8bb0d81b7ce4e5eb
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-27 10:22:11 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @RequestMapping
+    public String createEmployee(){
+        return "employee/employee-create";
+    }
+
+}
