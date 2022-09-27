086599047fdeefaa464120e226c52c99d73e80a0
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-27 14:50:20 -0500

    validation setup adjustments

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,6 +16,12 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+    //3 - @NotNull   //--> field cannot be null
+    //2 - @NotEmpty  //--> field cannot be empty ""
+    //1 - @NotBlank  //--> field cannot be blank "    "
+
+    @NotBlank
+    @Size(max=12, min=2)
     private String firstName;
     private String lastName;
 
